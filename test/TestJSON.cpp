4b82534cc7c0ae79aaa837ad1600146e8dede7d3
liwenjie <2915655152@qq.com>
2023-06-07 19:49:01 +0800

    update json test

--- test/TestJSON.cpp
+++ test/TestJSON.cpp
@@ -9,8 +9,12 @@ int main() {
 
     // ...
 
-    std::ifstream f("G:\\EDA\\EDA-ICTest\\morder-cpp\\data\\example.json");
+    // std::ifstream f("/home/liwenjie/workspace/MosqCPP/data/example.json");
+    // json data = json::parse(f);
+    // std::cout << data.at("pi") << std::endl;
+
+    std::ifstream f("/home/liwenjie/workspace/MosqCPP/data/func_test.json");
     json data = json::parse(f);
-    std::cout << data.at("pi") << std::endl;
+    std::cout << data.at("patterns").at("function_test")[0].at("cycles")[0].at("cycle") << std::endl;
     return 0;
 }
