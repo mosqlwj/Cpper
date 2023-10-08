c017132d1671fc5b3b430c989a2c89dc6c812e28
lwj <2915655152@qq.com>
2023-10-08 21:53:46 +0800

    add chap4

---
+++ test/modern_cpp/TestCustomTypes2.cpp
@@ -0,0 +1,12 @@
+// standord algorithms introduction
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include <vector>
+#include <algorithm>
+
+TEST_CASE("testing min_element") {
+    std::cout << "Hello" << std::endl;
+
+}
