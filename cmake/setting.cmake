dcb3257341e6ea7028a900e8801e7888a78bb566
lwj <2915655152@qq.com>
2023-12-29 23:22:53 +0800

    update cmake

---
+++ cmake/setting.cmake
@@ -0,0 +1,6 @@
+message(STATUS "Start setting.cmake")
+set(HOME_LEVELDB ${PROJECT_SOURCE_DIR}/leveldb)
+set(HOME_MOSQCPP ${PROJECT_SOURCE_DIR}/modern_cpp)
+
+
+message(STATUS "Finish setting.cmake")
