6cc6092733b44b20bc1b35a7b7776439d3d308dc
lwj <2915655152@qq.com>
2025-01-16 18:36:24 +0800

    update test modern cpp chap2

--- src/modern_cpp/test_modern_cpp.cpp
+++ src/modern_cpp/test_modern_cpp.cpp
@@ -11,6 +11,7 @@
 #include <type_traits>
 #include <algorithm>
 #include <format>
+#include <tuple>
 
 /**
  * 本cpp文件，用于测试吴咏伟的现代C++编程
@@ -384,4 +385,238 @@ TEST_CASE("Testing Modern CPP 2.2.1")  {
         std::cout << e << " ";
     }
     std::cout << std::endl;
+}
+
+/**
+* chap-2 2.2.2 测试初始化列表
+*/
+
+class MagicFoo {
+public:
+    MagicFoo(std::initializer_list<int> il) {
+        for (auto it = il.begin(); it != il.end(); ++it) {
+            vec_.push_back(*it);
+        }
+    }
+
+public:
+    std::vector<int> vec_;
+};
+
+TEST_CASE("Testing Modern CPP 2.2.2")  {
+    MagicFoo mf = {1, 2, 3, 4};
+    for (auto& e : mf.vec_) {
+        std::cout << e << " ";
+    };
+}
+
+/**
+* chap-2 2.2.3 测试结构化绑定
+*/
+
+auto foo_2_2_3() {
+    return std::make_tuple(1, 2.3, "hello");
+}
+
+TEST_CASE("Testing Modern CPP 2.2.3") {
+    auto [x, y, z] = foo_2_2_3();
+    std::cout << x << " " << y << " " << z << std::endl;
+}
+
+/**
+ * chap-2 2.3 测试类型推导
+ */
+
+template<typename R , typename T, typename U>
+R add(T x, U y) {
+    return x + y;
+}
+
+template<typename T, typename U>
+auto add2(T x, U y)-> decltype(x + y) {
+    return x + y;
+}
+
+
+template<typename T, typename U>
+auto add3(T x, U y) {
+    return x + y;
+}
+TEST_CASE("Testing Modern CPP 2.3") {
+    int x = add<int, int>(1, 2);
+    double y = add<double, int>(1.1, 2.1);
+}
+
+/**
+* chap-2 2.4 测试if constexpr
+*/
+
+template<typename T>
+auto print_type_info(const T& x) {
+    if constexpr (std::is_integral_v<T>) {
+        return x + 1;
+    } else {
+        return x + 0.1;
+    }
+}
+TEST_CASE("Testing Modern CPP 2.4")  {
+    std::cout << print_type_info(1) << std::endl;
+    std::cout << print_type_info(1.1) << std::endl;
+}
+
+/**
+* chap-2 2.5.3 测试using和typdef的区别
+*/
+
+template<typename T>
+class MagicType {
+public:
+  T x;
+};
+
+template<typename T>
+using MagicTypeInt = MagicType<T>;
+
+//template<typename T>
+//typename MagicType<T> MagicTypeInt;
+
+TEST_CASE("Testing Modern CPP 2.5.3")  {
+
+   MagicTypeInt<int> mt;
+}
+
+///////////////////////////////////////////////////////////////
+/**
+ * chap-2 2.5.4 测试变参模板
+ */
+TEST_CASE("Testing HOPL4 str")  {
+    const char* str1 = "hello";
+    std::cout << "addr str1:" << (void*)str1 << std::endl;
+    std::cout << "addr hello: " << &"hello" << std::endl;
+    std::string str2{"hello"};
+    std::cout << "addr str2:" << &str2 << std::endl;
+    std::cout << "addr str2.c_str(): "<< (void*)str2.c_str() << std::endl;
+
+}
+template<typename T, typename... Args>
+void printf(const char* s, const T& value, const Args&... args)
+{
+    while (*s) {
+        if (*s == '%' && *++s != '%') {
+            std::cout << value;
+            if constexpr (sizeof...(args) > 0) {
+                printf(++s, args...);  // 递归调用
+            } else {
+                ++s;  // 跳过占位符
+            }
+            return;
+        }
+        std::cout << *s++;
+    }
+    if constexpr (sizeof...(args) > 0) {
+        throw std::runtime_error("extra arguments provided to printf");
+    }
+}
+
+// 递归模板函数
+template<typename T0>
+void printf1(T0 value) {
+    std::cout << value << std::endl;
+}
+
+template<typename T, typename... Args>
+void printf1(T value, Args... args) {
+    std::cout << value << std::endl;
+    printf1(args...);
+}
+
+// 变参模板展开
+
+template<typename T, typename... Args>
+void printf2(T value, Args...args) {
+    std::cout << value << std::endl;
+    if constexpr (sizeof...(args) > 0) {
+        printf2(args...);
+    }
+}
+
+TEST_CASE("Testing HOPL4 printf")  {
+//    printf("hello %d %s %f", 1, "world", 3.14);
+    printf1(1, 2, "123", 1.23);
+    printf2(1, 2, "123", 1.23);
+}
+
+/**
+ * chap-2 2.5.5 测试折叠表达式
+ */
+
+template<typename... Args>
+auto sum(Args... args) {
+    return (args + ...);
+}
+
+//#include <iostream>
+//using namespace std;
+//
+//template<typename ... Args>
+//auto sum(Args... args)
+//{
+//    return (args + ...);
+//}
+//
+//#ifdef INSIGHTS_USE_TEMPLATE
+//template<>
+//int sum<int, int, int, int, int>(int __args0, int __args1, int __args2, int __args3, int __args4)
+//{
+//  return __args0 + (__args1 + (__args2 + (__args3 + __args4)));
+//}
+//#endif
+//
+//
+//int main()
+//{
+//    std::cout.operator<<(sum(1, 2, 3, 4, 5)).operator<<(std::endl);
+//    return 0;
+//}
+
+
+TEST_CASE("Testing Modern CPP 2.5.5")  {
+
+    std::cout << sum(1, 2, 3, 4, 5) << std::endl;
+}
+
+/**
+ * chap-2 2.5.6 测试非类型模板参数
+ */
+template<auto value>
+void foo_2_5_6() {
+    std::cout << value << std::endl;
+}
+
+TEST_CASE("Testing Modern CPP 2.5.6")  {
+    foo_2_5_6<100>();
+}
+
+/**
+* 2.6.1 测试强枚举类型
+*/
+
+enum class Status : int {
+    OK = 0,
+    ERROR = 1,
+};
+
+// C++ 17
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, const T& e) {
+    if constexpr (std::is_enum_v<T>) {
+        return stream << static_cast<std::underlying_type_t<T>>(e);
+    } else {
+        static_assert(false, "T must be an enum type!");
+    }
+}
+
+TEST_CASE("Testing Modern CPP 2.6.1")  {
+    std::cout << Status::OK << std::endl;
+    std::cout << static_cast<int>(Status::OK) << std::endl;
 }
